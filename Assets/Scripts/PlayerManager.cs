2f0665d7fc6331f76761cbdbdd16e18433d0ada3
Muhammed Ali Kurtulbaş <muhammedalikurtulbas@gmail.com>
2023-04-18 23:15:09 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 5f))
+        {
+            if(hit.collider.gameObject.tag == "Button")
+            {
+
+            }
+        }
+    }
+}
